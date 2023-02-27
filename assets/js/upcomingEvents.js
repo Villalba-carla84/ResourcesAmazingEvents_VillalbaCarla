d6f75efc16dcb410a19aa5ca2ab1bfba811f154a
Villalba-carla84 <carlavillalba84@hotmail.com>
2023-02-27 17:42:15 -0300

    segundo commit DOM

--- assets/js/upcomingEvents.js
+++ assets/js/upcomingEvents.js
@@ -11,12 +11,12 @@ for (let recorrido of data.events ) {
     if (fechaActual < Date.parse(recorrido.date)) {
     let card = document.createElement("div");
     card.classList.add("cards-container", "cards" ,"content");
-    card.innerHTML += 
+    card.innerHTML +=
     ` <div class="card">
     <img src="${recorrido.image}" >
               <h2>${recorrido.name}</h2>
               <div class= "content">
-              <p>${recorrido.description}.</p>
+              <p>${recorrido.description}</p>
               <a href="" class="btn btn-outline-info">price: ${recorrido.price}</a>
               <a href="./details.html" class="btn btn-outline-info">details</a>
 </div>
